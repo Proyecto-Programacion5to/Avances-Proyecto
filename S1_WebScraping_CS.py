a6b5e51ddd4adcd43e980b6a72e1567b675e28ba
Ana Jacinto <ana.bjacintoc@gmail.com>
2023-12-10 19:19:19 -0800

    Cambio de nombre al archivo.

--- S1_WebScraping_CS.py
+++ S1_WebScraping_CS.py
@@ -7,7 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import pandas as pd
 from mysql.connector import Error
-from ArchivoConstantes_CS import data_csv, data_limpieza
+from PF_ArchivoConstantes_CS import data_csv, data_limpieza
 from S2_Limpieza_CS import LimpiadorCSV
 from S3_Conexion_CS import DataBaseProyecto
 
@@ -36,14 +36,12 @@ def obtener_datos_producto(div):
     actualprice = div.find("p", attrs={"class": "sale-price-product"})
     oldprice = div.find("p", attrs={"class": "price-product"})
     payment = div.find("div", attrs={"class": "meses-telmex"})
-
     return {
         "Producto": productname.text if productname else "NA",
         "Precio Actual": actualprice.text if actualprice else 0,
         "Precio Anterior": oldprice.text if oldprice else 0,
         "Forma de pago": payment.text if payment else "NA",
     }
-
 def filtrado(navegador, filtro):
     dt = {"Clasificacion": [], "Producto": [], "Precio Actual": [], "Precio Anterior": [], "Forma de pago": []}
 
@@ -74,10 +72,41 @@ if __name__ == "__main__":
     try:
         cargar_pagina(navegador, "https://www.claroshop.com/")
         hacer_clic(navegador, By.XPATH, "//*[@id='__next']/div[1]/header/div[1]/div[2]/div/nav[2]/ul[1]/li[2]/a")
-        data_df = filtrado(navegador, ["celulares-y-telefonia-li", "electronica-y-tecnologia-li", "videojuegos-li", "hogar-y-jardin-li", "deportes-y-ocio-li", "ferreteria-y-autos-li", "salud-belleza-y-cuidado-personal-li"])
+        data_df = filtrado(navegador, ["celulares-y-telefonia-li", "electronica-y-tecnologia-li", "videojuegos-li", "hogar-y-jardin-li",
+                             "deportes-y-ocio-li", "ferreteria-y-autos-li", "salud-belleza-y-cuidado-personal-li"])
         data_df.to_csv(data_csv)
         cerrar_navegador(navegador)
         print("Web scraping realizado exitosamente.")
 
-    except: Error as error:
-    print("Error al realizar el web scraping:", error)
+    except Error as error:
+        print("Error al realizar el web scraping:", error)
+
+    try:
+        archivo_csv = data_csv
+        limpieza = LimpiadorCSV(archivo_csv)
+        limpieza.limpieza_duplicados()
+        limpieza.limpieza_nulos()
+        limpieza.limpieza_de_precio()
+        limpieza.extraer_valores_numericos('Forma de pago')
+        limpieza.limpiar_forma_de_pago()
+        limpieza.signo_de_pesos()
+        limpieza.precio_anterior_cero()
+        limpieza.cambio_nombre()
+        limpieza.guardar_csv(data_limpieza)
+        print("Limpieza de datos realizado exitosamente.")
+
+    except Error as error:
+        print("Error al realizar la limpieza del archivo csv:", error)
+
+    try:
+        db = DataBaseProyecto()
+        db.conectar()
+        db.crear_base_datos()
+        db.crear_tablas()
+        db.insertar_datos()
+        db.crearquerys()
+        db.cerrar_conexion()
+        print("Consulta de datos realizada exitosamente.")
+
+    except Error as error:
+        print("Error al hacer la conexion con la base de datos:", error)
